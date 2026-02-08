d41ea89b62838d38188a2154c8ecf01650734139
Artemis-Ash <artemis.ash@city.ac.uk>
2026-02-08 14:01:44 +0000

    main tf

---
+++ infra/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "github-cicd-486812"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
